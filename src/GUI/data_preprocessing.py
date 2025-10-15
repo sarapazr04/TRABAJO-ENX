f778b1d3671a3dd43d3fb646db4a7cddee91aac1
Laura Castro Exposito <l.castro.exposito@udc.es>
2025-10-15 15:06:20 +0200

    feat: Añadir clase RadioButtonFrame

--- src/GUI/data_preprocessing.py
+++ src/GUI/data_preprocessing.py
@@ -4,6 +4,39 @@ from components import (
     Panel, LoadingIndicator
 )
 
+class RadioButtonFrame(ctk.CTkFrame):
+    def __init__(self, master, title, values, input_box):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.input_box = input_box
+        self.variable = ctk.StringVar(value="")
+
+        if title is not None:
+            self.title = ctk.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+            self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        for i, value in enumerate(self.values):
+            if i in self.input_box:
+                input_button_frame = ctk.CTkFrame(self, fg_color = "transparent")
+                input_button_frame.grid(row=i + 1, column=0, padx=0, pady=0, sticky="w")
+
+                radiobutton = ctk.CTkRadioButton(input_button_frame, text=value, value=value, variable=self.variable)
+                radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+
+                entry = ctk.CTkEntry(input_button_frame, placeholder_text="")
+                entry.grid(row= i+1, column = 1, padx=10, pady=(10, 0), sticky="w")
+            else:
+                radiobutton = ctk.CTkRadioButton(self, text=value, value=value, variable=self.variable)
+                radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
+
 def _create_preprocessing_panel(master):
     preprocessing_panel = Panel(master, "Preprocesamiento de datos")
     preprocessing_panel.pack(
@@ -17,23 +50,21 @@ def _create_NA_table(master):
     pass
 
 def _create_substitute_options(master):
-    pass
+    app.radiobutton_frame = RadioButtonFrame(app, "Opciones", values=["Eliminar", "Media", "Mediana", "Constante"], input_box= [3])
+    app.radiobutton_frame.pack(fill="both", expand= True, padx=10, pady=(10, 0))
 
 
 def combobox_callback(choice):
     print("combobox dropdown clicked:", choice)
 
 
+
+
 app = ctk.CTk()
-
-'''
-combobox = ctk.CTkComboBox(app, values=["option 1", "option 2"],
-                                     command=combobox_callback)
-combobox.set("option 2")
-
-
-combobox.pack(padx=20, pady=20)
-'''
+app.geometry("400x350")
 _create_preprocessing_panel(app)
+
+
+
 
 app.mainloop()
